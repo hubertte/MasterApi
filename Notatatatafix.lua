7ced9f27fa9e0ef1ca64bb315e4f1bcbdf207de1
hubertte <121490817+hubertte@users.noreply.github.com>
2024-08-22 12:07:30 +0200

    Create Notatatatafix.lua

---
+++ Notatatatafix.lua
@@ -0,0 +1,3 @@
+local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/IceMinisterq/Notification-Library/Main/Library.lua"))()
+
+NotificationLibrary:SendNotification("MasterAPI Message", "Im injected!", 5)
